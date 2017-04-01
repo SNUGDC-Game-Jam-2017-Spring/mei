86ace0997d360ae91c606e4c1d1f1bf0d32a77c4
dohoon122 <dohoon122@naver.com>
2017-04-02 01:22:29 +0900

    overshooting fixed

--- Assets/scripts/CatJumper.cs
+++ Assets/scripts/CatJumper.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class CatJumper : MonoBehaviour {
 
-    public float jumpPower1 = 30.0f;
+    public float jumpPower1 = 10.0f;
     public float xAxisVelocity = 0.0f;
     public float yAxisVelocity = 0.0f;
-    public float maxXAxisVelocity = 10.0f;
+    public float maxXAxisVelocity = 5.0f;
     public float maxYAxisVelocity = 10.0f;
+
 
     public Vector2 delta = Vector2.zero;
     public Vector3 lastMousePosition =Vector2.zero;
     public Vector3 MousePosition = Vector2.zero;
     public static Vector2[] recording = new Vector2[5];
     public Vector2 Force=Vector2.zero;
-    public Vector2 ResistForce = Vector2.zero;
+    public Vector2 catPosition = Vector2.zero;
 
 
 
@@ -43,6 +44,8 @@ public class CatJumper : MonoBehaviour {
             recording[i] = recording[i + 1];
         }
 
+        catPosition = rb.position;
+
         //튕기는 Force
         if (Input.GetMouseButtonDown(0))
         {
@@ -62,11 +65,17 @@ public class CatJumper : MonoBehaviour {
             {
                 delta = Vector2.down * delta.y + delta;
             }
-            rb.AddForce(delta * jumpPower1);
-            rb.AddForce(ResistForce);
+            if (delta.x >= 0)
+            {
+                rb.AddForce((Vector2.right * Mathf.Pow((delta.x), 0.5f) + Vector2.up * Mathf.Pow((delta.y), 0.5f)) * jumpPower1);
+            }
+            else if (delta.x < 0)
+            {
+                rb.AddForce((-Vector2.right * Mathf.Pow((-delta.x), 0.5f) + Vector2.up * Mathf.Pow((delta.y), 0.5f)) * jumpPower1);
+            }
+            
         }
-
-        Force = delta * jumpPower1;
+        
 
         //속력 제한
         if(yAxisVelocity >= maxYAxisVelocity)
@@ -82,17 +91,6 @@ public class CatJumper : MonoBehaviour {
             rb.velocity = Vector2.left * maxXAxisVelocity + Vector2.up * rb.velocity.y;
         }
 
-        //저항력
-        ResistForce = Vector2.left * xAxisVelocity + Vector2.down * yAxisVelocity * yAxisVelocity;
-        if (Mathf.Abs(xAxisVelocity) >= maxYAxisVelocity)
-        {
-            ResistForce = Vector2.left * Force.x + Vector2.up * Force.y;
-        }
-        if(yAxisVelocity >= maxYAxisVelocity)
-        {
-            ResistForce = Vector2.right * Force.x + Vector2.down * Force.y;
-        }
 
-        rb.AddForce(Vector2.zero);
     }
 }
