f31b53f6f48f5b2306d093390ba9a2012a15cd76
dohoon122 <dohoon122@naver.com>
2017-04-02 04:02:01 +0900

    cat collider 구현

---
+++ Assets/CatCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatCollider : MonoBehaviour
+{
+    void Update()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        var yAxisVelocity = rb.velocity.y;
+
+        if (yAxisVelocity >= 0)
+        {
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("cat"), LayerMask.NameToLayer("platform"), true);
+        }
+        else if (yAxisVelocity < 0)
+        {
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("cat"), LayerMask.NameToLayer("platform"), false);
+        }
+    }
+}
