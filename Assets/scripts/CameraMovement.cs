b6d0547f9cfcacde5a41dae000b9cbd4bd89eb25
dohoon122 <dohoon122@naver.com>
2017-04-02 09:06:54 +0900

    camera movement 추가

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{ 
+    public GameObject cat;
+    Vector3 cameraPosition;
+    public float HeightRecorder;
+
+
+    void Start()
+    {
+        cameraPosition = transform.position;
+        cameraPosition.y = 0.0f;
+        HeightRecorder = 0.0f;
+    }
+
+
+    void Update()
+    {
+        var catHeight = cat.transform.position.y;
+
+        if (catHeight > 3.0f)
+        {
+            if (catHeight - cameraPosition.y > 3.0f)
+            {
+                cameraPosition.y = catHeight - 3.0f;             
+                this.transform.position = cameraPosition;
+                HeightRecorder = catHeight;
+            }
+
+            else
+            {
+                cameraPosition.y = HeightRecorder - 3.0f;
+                this.transform.position = cameraPosition;
+            }
+        }
+        else
+        {
+            this.transform.position = cameraPosition;
+        }
+    }
+}
