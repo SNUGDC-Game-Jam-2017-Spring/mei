970bec99ebb2752f120902662055cc8a00d6b8a7
Phillip Chang <lasagnaphil@snu.ac.kr>
2017-04-02 18:03:58 +0900

    고양이 죽이기 스크립트 추가

---
+++ Assets/scripts/CatKiller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatKiller : MonoBehaviour
+{
+    [SerializeField] private PlatformSpawner platformSpawner;
+    public bool catKilled;
+
+	void Update () {
+	    if (!catKilled)
+	    {
+	        if (transform.position.y < platformSpawner.MinPlatformHeight)
+	        {
+	            Debug.Log("Cat killed!");
+	            catKilled = true;
+	        }
+	    }
+	}
+}
