13a388af0efd1c3cbc79ec36aa0b6565effefd04
kjyong1983 <kjyong1983@gmail.com>
2017-04-02 17:06:59 +0900

    UI에 스코어 표시 기능 추가.

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GameObject cat;
+    float catHeight = 0;
+    public Text score;
+
+	// Use this for initialization
+	void Start () {
+        score = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (cat.transform.position.y > catHeight)
+        {
+            catHeight = cat.transform.position.y;
+        }
+
+        score.text = "Score : " + catHeight;
+
+
+    }
+}
